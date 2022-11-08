3daf4da74d36a34f8fa04d64a28d9f3b7b5f96d2
gumgum <ronaldovolkert@gmail.com>
2022-11-08 22:33:15 +0700

    add index.js

---
+++ index.js
@@ -0,0 +1,34 @@
+
+//soal 1
+let a = 4  // bagian umur bapaknua
+let b = 1  // bagian umur anaknya
+let c = 80 //jumlah umur anak sama bapak
+let d = (c/(a+b)) // total umur dibagi jumlah "total bagian umur"
+
+
+// soal 2
+let x = "super sekali"
+let y = x.length;
+
+
+//soal 3
+let j = new Date;
+const TodaysDate = (j.getDate());
+const TodaysMonth = (j.getMonth()+1);
+const TodaysYear = (j.getFullYear());
+const TomorrowDate = (j.getDate()+1);
+const TomorrowMonth = (j.getMonth()+2);
+const TomorrowYear = ((j.getFullYear()+1));
+const YesterDate = (j.getDate()-1);
+const YesterMonth = (j.getMonth());
+const YesterYear = ((j.getFullYear()-1));
+const k = ("Todays Date is" , TodaysDate, TodaysMonth, TodaysYear);
+const l = ("Yesterday in last month one year ago, the date was", YesterDate, YesterMonth, YesterYear);
+const m = ("Tomorrow in next month one year later, the date was", TomorrowDate, TomorrowMonth, TomorrowYear);
+
+
+// console log ()
+// d > jawaban soal no 1
+// y > jawaban soal no 2
+// k,l,m > jawaban soal no 3 tergantung kebutuhan
+console.log ()
